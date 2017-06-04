c5ed8ad0f03869285c8cc9b88495c758823ee05d
Jeron Aldaron Lau <jeron.lau@plopgrizzly.tech>
2017-06-04 12:49:20 -0500

    Implemented ops for pointer math on VoidPointer.

--- src/void_pointer.rs
+++ src/void_pointer.rs
@@ -6,19 +6,32 @@
 
 //! This module is for repurposing memory returned from or sent to FFI.
 
+use core::ops::{Add, Sub, Not, BitAnd, BitOr, BitXor, Shr, Shl};
+
+#[cfg(target_pointer_width = "32")]
+type NativePtr = u32;
+
+#[cfg(target_pointer_width = "64")]
+type NativePtr = u64;
+
 /// A type that represents a void* in C.
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq)]
 pub struct VoidPointer {
-	#[cfg(target_pointer_width = "32")]
-	native: u32,
-	#[cfg(target_pointer_width = "64")]
-	native: u64,
+	native: NativePtr,
 }
 
 /// Equivalent of NULL in C.
 pub const NULL : VoidPointer = VoidPointer { native: 0 };
 
+impl VoidPointer {
+	/// Return the pointer as an integer.
+	#[inline(always)]
+	pub fn as_int(&self) -> NativePtr {
+		self.native
+	}
+}
+/*
 /// A trait used for casting the void pointer to other pointer types.
 pub trait VoidPointerCast<T> {
 	/// Cast a VoidPointer to a native pointer of any type.
@@ -41,6 +54,86 @@ impl<T> VoidPointerCast<T> for VoidPointer {
 	fn cast(&self) -> *mut T {
 		unsafe {
 			*(&self as *const _ as *const *mut _)
+		}
+	}
+}*/
+
+impl Add<u32> for VoidPointer {
+	type Output = VoidPointer;
+
+	fn add(self, other: u32) -> VoidPointer {
+		VoidPointer {
+			native: self.native + (other as NativePtr)
+		}
+	}
+}
+
+impl Sub<u32> for VoidPointer {
+	type Output = VoidPointer;
+
+	fn sub(self, other: u32) -> VoidPointer {
+		VoidPointer {
+			native: self.native - (other as NativePtr)
+		}
+	}
+}
+
+impl BitAnd<u32> for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn bitand(self, other: u32) -> VoidPointer {
+		VoidPointer {
+			native: (self.native as u32 & other) as NativePtr
+		}
+	}
+}
+
+impl BitOr<u32> for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn bitor(self, other: u32) -> VoidPointer {
+		VoidPointer {
+			native: (self.native as u32 | other) as NativePtr
+		}
+	}
+}
+
+impl BitXor<u32> for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn bitxor(self, other: u32) -> VoidPointer {
+		VoidPointer {
+			native: (self.native as u32 ^ other) as NativePtr
+		}
+	}
+}
+
+impl Shr<usize> for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn shr(self, rhs: usize) -> VoidPointer {
+		VoidPointer {
+			native: self.native >> rhs
+		}
+	}
+}
+
+impl Shl<usize> for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn shl(self, rhs: usize) -> VoidPointer {
+		VoidPointer {
+			native: self.native << rhs
+		}
+	}
+}
+
+impl Not for VoidPointer {
+	type Output = VoidPointer;
+	
+	fn not(self) -> VoidPointer {
+		VoidPointer {
+			native: !self.native
 		}
 	}
 }
